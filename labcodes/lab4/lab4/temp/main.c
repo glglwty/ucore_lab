17751766dade25309e742978bd2489ced102098d
glglwty <glglwty@gmail.com>
2015-03-19 16:34:03 +0800

    merge lab1~3 to lab4

---
+++ labcodes/lab4/lab4/temp/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  temp
+//
+//  Created by glglwty on 3/19/15.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
